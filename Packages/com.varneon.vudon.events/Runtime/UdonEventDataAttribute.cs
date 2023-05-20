3aa3b2301510833aef5dbb8923e0204d82c62f61
Varneon <me@varneon.com>
2023-05-19 22:35:39 -0700

    Add UdonEventDataAttribute

---
+++ Packages/com.varneon.vudon.events/Runtime/UdonEventDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.UdonEvents
+{
+    public class UdonEventDataAttribute : PropertyAttribute
+    {
+        public readonly string EventDataFieldName;
+
+        public UdonEventDataAttribute(string eventDataFieldName)
+        {
+            EventDataFieldName = eventDataFieldName;
+        }
+    }
+}
